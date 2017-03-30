3a959221247e1bf7bfaac1d259fcb5f7ba361162
Nicholas Oshukany <ozzie347@hotmail.com>
2017-03-30 10:42:24 -0400

    Created StartForm and SelectForm. Added buttons and functionality to StartForm

---
+++ COMP1004-Assignment4/StartForm.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace COMP1004_Assignment4
+{
+    partial class StartForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.newOrderButton = new System.Windows.Forms.Button();
+            this.savedOrderButton = new System.Windows.Forms.Button();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // newOrderButton
+            // 
+            this.newOrderButton.Location = new System.Drawing.Point(81, 211);
+            this.newOrderButton.Name = "newOrderButton";
+            this.newOrderButton.Size = new System.Drawing.Size(220, 40);
+            this.newOrderButton.TabIndex = 0;
+            this.newOrderButton.Text = "Start a New Order";
+            this.newOrderButton.UseVisualStyleBackColor = true;
+            this.newOrderButton.Click += new System.EventHandler(this.newOrderButton_Click);
+            // 
+            // savedOrderButton
+            // 
+            this.savedOrderButton.Location = new System.Drawing.Point(81, 254);
+            this.savedOrderButton.Name = "savedOrderButton";
+            this.savedOrderButton.Size = new System.Drawing.Size(220, 40);
+            this.savedOrderButton.TabIndex = 1;
+            this.savedOrderButton.Text = "Open a Saved Order";
+            this.savedOrderButton.UseVisualStyleBackColor = true;
+            // 
+            // exitButton
+            // 
+            this.exitButton.Location = new System.Drawing.Point(81, 297);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(220, 40);
+            this.exitButton.TabIndex = 2;
+            this.exitButton.Text = "Exit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // StartForm
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.ClientSize = new System.Drawing.Size(382, 353);
+            this.ControlBox = false;
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.savedOrderButton);
+            this.Controls.Add(this.newOrderButton);
+            this.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.Name = "StartForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Welcome to Dollar Computer!";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button newOrderButton;
+        private System.Windows.Forms.Button savedOrderButton;
+        private System.Windows.Forms.Button exitButton;
+    }
+}
+
---
+++ COMP1004-Assignment4/StartForm.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace COMP1004_Assignment4
+{
+    public partial class StartForm : Form
+    {
+        public StartForm()
+        {
+            InitializeComponent();
+        }
+
+        private void newOrderButton_Click(object sender, EventArgs e)
+        {
+            SelectForm selectForm = new SelectForm();
+
+            Hide();
+
+            selectForm.Show();
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
